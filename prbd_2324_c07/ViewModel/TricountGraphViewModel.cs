25f97aa5d7397c496ef23a4957a9eef4df4cc134
Quentin <quentinmaes96@gmail.com>
2024-05-19 17:29:56 +0200

    feat : Tricount Graph, création des fichiers

--- prbd_2324_c07/ViewModel/TricountGraphViewModel.cs
+++ prbd_2324_c07/ViewModel/TricountGraphViewModel.cs
@@ -1,7 +1,9 @@
-﻿using prbd_2324_c07.Model;
+﻿using Azure;
+using prbd_2324_c07.Model;
 using PRBD_Framework;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,36 @@ namespace prbd_2324_c07.ViewModel
 {
     public class TricountGraphViewModel : ViewModelBase<User, PridContext>
     {
+        private Tricount _tricount;
+        public Tricount Tricount {
+            get => _tricount;
+            set => SetProperty(ref _tricount, value);
+        }
 
-        public TricountGraphViewModel() { }
+        private ObservableCollection<TricountGraphBarViewModel> _tricountGraphBars;
+
+        public ObservableCollection<TricountGraphBarViewModel> TricountGraphBars {
+            get => _tricountGraphBars;
+            set => SetProperty(ref _tricountGraphBars, value);
+        }
+
+
+        public TricountGraphViewModel(Tricount tricount) {
+            Tricount = tricount;
+            OnRefreshData();
+        }
+
+
+        protected override void OnRefreshData() {
+
+            var participants = Tricount.ParticipantTricount()
+                 .Select(par => par.User)
+                 .Distinct();
+
+            TricountGraphBars = new ObservableCollection<TricountGraphBarViewModel>(participants.Select(user =>
+                new TricountGraphBarViewModel(user, Tricount.GetUserBalance(user), user == Tricount.Creator ? true : false)
+            ));
+
+        }
     }
 }
